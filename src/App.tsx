b1d3125eaf11fdaf990c3603747430c333b64da8
Peter Irlam <irlampeter@gmail.com>
2023-03-03 12:50:40 +0000

    Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,117 @@
-import React from 'react';
+import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
 
+import logo from "./logo.svg";
+// import './App.css';
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
+//TYPES
+import { QuestionState, Difficulty } from "./API";
+// STYLES
+import { GlobalStyle, Wrapper } from "./App.styles";
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  // Question number user is on
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  // async fn as we make the api call frrom here
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: any) => {
+    if (!gameOver) {
+      // get users answer from QuestionCard.tsx <button value={answer}...>
+      const answer = e.currentTarget.value;
+      // check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      // save answer in the array for user answers
+      const AnswerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, AnswerObject]);
+    }
+  };
+  //fn that triggers when user selects nx question
+  const nextQuestion = () => {
+    // move on to next Q if not last Q
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Quiz</h1>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>Computer Science Trivia</h1>
+        {/* Display start btn only if game over or user is on last question */}
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className='start' onClick={startTrivia}>
+            Test Your Knowledge!
+          </button>
+        ) : null}
+        {/* Only show score if not game over man! */}
+        {!gameOver ? <p className='score'>Score: {score}</p> : null}
+        {/* Show loading questions only when loading Qs */}
+        {loading && <p>Loading Questions...</p>}
+        {/* Only disp question card if not loading and not game over */}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {/* Only show nx Q btn if not game over, not loading, if user has not yet given an answer & if we're not on the last question */}
+        {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
